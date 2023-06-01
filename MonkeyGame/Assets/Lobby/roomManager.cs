4d5d9e22bd57017af8f803c0e80d87d31d2c12a5
i7zaifa <61591270+huzaifahoho18@users.noreply.github.com>
2023-06-01 19:47:02 +0200

    test

--- MonkeyGame/Assets/Lobby/roomManager.cs
+++ MonkeyGame/Assets/Lobby/roomManager.cs
@@ -16,12 +16,6 @@ public class roomManager : MonoBehaviour
     float StartTime;
 
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
