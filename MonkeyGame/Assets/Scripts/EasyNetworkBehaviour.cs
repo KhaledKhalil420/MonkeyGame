9041f973eaee567641bb8eee6a86d9b40153177f
KhaledKhalil69 <108684316+KhaledKhalil420@users.noreply.github.com>
2023-06-06 06:26:38 +0300

    1

--- MonkeyGame/Assets/Scripts/EasyNetworkBehaviour.cs
+++ MonkeyGame/Assets/Scripts/EasyNetworkBehaviour.cs
@@ -4,28 +4,6 @@ using UnityEngine;
 
 public class EasyNetworkBehaviour : NetworkBehaviour
 {
-    #region Destroy Object
-
-    public void DestroyNObject(GameObject _gameObject)
-    {
-        Destroy_ServerRpc(_gameObject);
-    }
-
-    [ServerRpc(RequireOwnership = false)]
-    void Destroy_ServerRpc(GameObject _gameObject)
-    {
-        Destroy_ClientRpc(_gameObject);
-    }
-
-    [ClientRpc]
-    public void Destroy_ClientRpc(GameObject _gameObject)
-    {
-        _gameObject.GetComponent<NetworkObject>().Despawn(false);
-        Destroy(_gameObject);
-    }
-
-    #endregion
-
     #region Go Scene
     
     public void LoadSceneRpc(string LevelName)
