c7590050d3e2c311dcb6b77f75512098739c7fc5
i7zaifa <61591270+huzaifahoho18@users.noreply.github.com>
2023-06-06 18:46:55 +0200

    outLinFix

---
+++ MonkeyGame/Assets/jumpingPad.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class jumpingPad : MonoBehaviour
+{
+    public float jumpForce;
+
+    /// <summary>
+    /// Sent when another object enters a trigger collider attached to this
+    /// object (2D physics only).
+    /// </summary>
+    /// <param name="other">The other Collider2D involved in this collision.</param>
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            other.GetComponent<Rigidbody2D>().AddForce(transform.up * jumpForce);
+        }
+    }
+}
